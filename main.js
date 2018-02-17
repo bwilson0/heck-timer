b73a0a6e4eae8b7274abfb7a2f2a2735831a1cb2
bwilson0 <bwilson0@users.noreply.github.com>
2018-02-17 19:35:45 +1100

    asdf

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
-const electron = require('electron');
-const url = require('url');
-const path = require('path');
-
-const {app, BrowserWindow, ipcMain} = electron;
-
-let mainWindow;
-
-// listen for app to be ready
-app.on('ready', function(){
-  //create mainWindow
-  mainWindow = new BrowserWindow({});
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'mainWindow.html'),
-    protocol: 'file',
-    slashes: true
-  }));
-});
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);
